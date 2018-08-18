47009890f5a5286d0f65b326926f8bcd3c37e313
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-18 15:18:55 +0200

    Simplified

--- qtmain.cpp
+++ qtmain.cpp
@@ -11,17 +11,6 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   ribi::brar::QtConceptMapTest t;
-
-  //Center the QtConceptMap
-  if (std::rand() < -1)
-  {
-    const QRect screen = QApplication::desktop()->screenGeometry();
-    t.m_qtconceptmap->setGeometry(screen.adjusted(364, 64, -364, -64));
-    t.m_qtconceptmap->move(screen.center() - t.m_qtconceptmap->rect().center());
-    t.m_qtconceptmap->setGeometry(
-      t.m_qtconceptmap->geometry().adjusted(0, 0, 0, -264)
-    );
-  }
   t.m_qtconceptmap->showFullScreen();
   return a.exec();
 }
