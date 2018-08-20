99737a03b17c845c389b38b7d2eff6a7552edfca
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-20 16:29:56 +0200

    Use random seed on Travis

--- qtmain.cpp
+++ qtmain.cpp
@@ -1,10 +1,21 @@
 #include <QApplication>
 
+#include <iostream>
+
+#include "conceptmaphelper.h"
 #include "qtbrainweaverruffianqtconceptmap_test.h"
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+
+  if (ribi::cmap::OnTravis())
+  {
+    //Randomize timer
+    const int seed = std::time(0);
+    std::cout << "RNG seed: " << seed;
+    std::srand(seed);
+  }
   ribi::brar::QtConceptMapTest t;
   t.m_qtconceptmap->showFullScreen();
   return a.exec();
