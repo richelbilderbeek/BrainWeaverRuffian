d049cbd14111d1309392b15b10f0ba7b226ab5f0
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-21 14:06:12 +0200

    Show QUndoView

--- qtmain.cpp
+++ qtmain.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include <QDebug>
+#include <QUndoView>
 
 #include "conceptmaphelper.h"
 #include "qtbrainweaverruffianqtconceptmap_test.h"
@@ -25,5 +26,8 @@ int main(int argc, char *argv[])
   ribi::brar::QtConceptMapTest t;
   t.m_qtconceptmap->show();
   t.m_qtconceptmap->setGeometry(32, 32, 1200, 600);
+  QUndoView * const undo = new QUndoView(&t.m_qtconceptmap->GetUndo());
+  undo->show();
+  undo->setGeometry(32, 632, 1200, 200);
   return a.exec();
 }
